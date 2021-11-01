11cf800b4f7186f39f328b7db8a2fa6306150561
lmochan <91670750+lmochan@users.noreply.github.com>
2021-11-01 16:05:05 +0000

    bash script for deploying

---
+++ upgradeRancher.sh
@@ -0,0 +1,228 @@
+curl -u "$RANCHER_ACCESS_KEY":"$RANCHER_SECRET_KEY" \
+-X POST \
+-H 'Content-Type: application/json' \
+-d '{
+      {
+   "type":"serviceUpgrade",
+   "inServiceStrategy":{
+      "type":"inServiceUpgradeStrategy",
+      "batchSize":1,
+      "intervalMillis":2000,
+      "launchConfig":{
+         "type":"launchConfig",
+         "blkioWeight":null,
+         "capAdd":[
+            
+         ],
+         "capDrop"[
+            
+         ],
+         "cgroupParent":null,
+         "count":null,
+         "cpuCount":null,
+         "cpuPercent":null,
+         "cpuPeriod":null,
+         "cpuQuota":null,
+         "cpuRealtimePeriod":null,
+         "cpuRealtimeRuntime":null,
+         "cpuSet":null,
+         "cpuSetMems":null,
+         "cpuShares":null,
+         "dataVolumes":[
+            
+         ],
+         "dataVolumesFrom":[
+            
+         ],
+         "description":null,
+         "devices":[
+            
+         ],
+         "diskQuota":null,
+         "dns":[
+            
+         ],
+         "dnsSearch"[
+            
+         ],
+         "domainName":null,
+         "healthInterval":null,
+         "healthRetries":null,
+         "healthTimeout":null,
+         "hostname":null,
+         "imageUuid":"docker:lchanif/people-frontend:latest",
+         "instanceTriggeredStop":"stop",
+         "ioMaximumBandwidth":null,
+         "ioMaximumIOps":null,
+         "ip":null,
+         "ip6":null,
+         "ipcMode":null,
+         "isolation":null,
+         "kernelMemory":null,
+         "kind":"container",
+         "labels":{
+            "io.rancher.container.pull_image":"always"
+         },
+         "logConfig":{
+            "type":"logConfig",
+            "config":{
+               
+            },
+            "driver":null
+         },
+         "memory":null,
+         "memoryMb":null,
+         "memoryReservation":null,
+         "memorySwap":null,
+         "memorySwappiness":null,
+         "milliCpuReservation":null,
+         "networkMode":"managed",
+         "oomScoreAdj":null,
+         "pidMode":null,
+         "pidsLimit":null,
+         "ports":[
+            
+         ],
+         "privileged":false,
+         "publishAllPorts":false,
+         "readOnly":false,
+         "requestedIpAddress":null,
+         "runInit":false,
+         "secrets":[
+            
+         ],
+         "shmSize":null,
+         "startOnCreate":true,
+         "stdinOpen":true,
+         "stopSignal":null,
+         "stopTimeout":null,
+         "system":false,
+         "tty":true,
+         "user":null,
+         "userdata":null,
+         "usernsMode":null,
+         "uts":null,
+         "version":"4c90c6b8-758e-4fb7-8cd1-189fcc92a91b",
+         "volumeDriver":null,
+         "workingDir":null,
+         "dataVolumesFromLaunchConfigs":[
+            
+         ],
+         "networkLaunchConfig":null,
+         "vcpu":1,
+         "drainTimeoutMs":0
+      },
+      "previousLaunchConfig":{
+         "type":"launchConfig",
+         "blkioWeight":null,
+         "capAdd":[
+            
+         ],
+         "capDrop":[
+            
+         ],
+         "cgroupParent":null,
+         "count":null,
+         "cpuCount":null,
+         "cpuPercent":null,
+         "cpuPeriod":null,
+         "cpuQuota":null,
+         "cpuRealtimePeriod":null,
+         "cpuRealtimeRuntime":null,
+         "cpuSet":null,
+         "cpuSetMems":null,
+         "cpuShares":null,
+         "dataVolumes":[
+            
+         ],
+         "dataVolumesFrom":[
+            
+         ],
+         "description":null,
+         "devices":[
+            
+         ],
+         "diskQuota":null,
+         "dns":[
+            
+         ],
+         "dnsSearch":[
+            
+         ],
+         "domainName":null,
+         "healthInterval":null,
+         "healthRetries":null,
+         "healthTimeout":null,
+         "hostname":null,
+         "imageUuid":"docker:lchanif/people-frontend:latest",
+         "instanceTriggeredStop":"stop",
+         "ioMaximumBandwidth":null,
+         "ioMaximumIOps":null,
+         "ip":null,
+         "ip6":null,
+         "ipcMode":null,
+         "isolation":null,
+         "kernelMemory":null,
+         "kind":"container",
+         "labels":{
+            "io.rancher.container.pull_image":"always"
+         },
+         "logConfig":{
+            "type":"logConfig",
+            "config":{
+               
+            },
+            "driver":null
+         },
+         "memory":null,
+         "memoryMb":null,
+         "memoryReservation":null,
+         "memorySwap":null,
+         "memorySwappiness":null,
+         "milliCpuReservation":null,
+         "networkMode":"managed",
+         "oomScoreAdj":null,
+         "pidMode":null,
+         "pidsLimit":null,
+         "ports":[
+            
+         ],
+         "privileged":false,
+         "publishAllPorts":false,
+         "readOnly":false,
+         "requestedIpAddress":null,
+         "runInit":false,
+         "secrets":[
+            
+         ],
+         "shmSize":null,
+         "startOnCreate":true,
+         "stdinOpen":true,
+         "stopSignal":null,
+         "stopTimeout":null,
+         "system":false,
+         "tty":true,
+         "user":null,
+         "userdata":null,
+         "usernsMode":null,
+         "uts":null,
+         "version":"e60233c2-6958-418b-9df1-b106d2e7b9df",
+         "volumeDriver":null,
+         "workingDir":null,
+         "dataVolumesFromLaunchConfigs":[
+            
+         ],
+         "networkLaunchConfig":null,
+         "vcpu":1,
+         "drainTimeoutMs":0
+      },
+      "previousSecondaryLaunchConfigs":[
+         
+      ],
+      "secondaryLaunchConfigs":[
+         
+      ],
+      "startFirst":false
+   },
+   "toServiceStrategy":null
+}' 'https://18.170.150.5:8080/v2-beta/projects/1a5/services/1s20/?action=upgrade'
